5c86b80b5cb6ec06dd049dee984848586c2c648b
Saniar EDUARDO <saniar.eduardo@aifrance.com>
2022-10-26 03:36:08 +0200

    fuck

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 export DRONE_GITHUB_CLIENT_ID=ac3f6f6c6300557bc4e7
 export DRONE_GITHUB_CLIENT_SECRET=7cd60dfaaf1b7e8112ded44bed2c4bc30068ad86
 export DRONE_GITHUB_ADMIN=SaniarVM
-export DRONE_SERVER_HOST=8ea8-217-128-52-119.eu.ngrok.io
+export DRONE_SERVER_HOST=5dae-217-128-52-119.eu.ngrok.io
 
 export HOSTNAME=$(hostname)
 export DRONE_RPC_SECRET="$(echo ${HOSTNAME} | openssl dgst -md5 -hex)"
